c8486776263a9c5059b72c7d11547981421e5983
Alex <jinghaowang1997@gmail.com>
2025-09-13 17:12:03 -0400

    feat: update CallToAction component with new heading and adjust layout for better presentation

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -4,10 +4,10 @@ import { Rocket } from "lucide-react";
 export default function CallToAction() {
   return (
     <section className="pt-32 pb-60 px-4">
-      <div className="max-w-5xl mx-auto text-center space-y-12">
+      <div className="max-w-3xl mx-auto text-center space-y-12">
         {/* Heading */}
-        <h2 className="text-4xl lg:text-6xl font-bold text-foreground elegant-heading">
-          Your Journey to Mastery Starts Here
+        <h2 className="text-4xl lg:text-6xl font-bold text-foreground elegant-heading py-4">
+          Have more questions? I&apos;m happy to talk!
         </h2>
 
         {/* Button */}
@@ -18,6 +18,9 @@ export default function CallToAction() {
           Book A Free Trial Lesson
           <Rocket className="ml-2 w-6 h-6" />
         </Button>
+        <p className="text-md text-center">
+          Your first lesson is completely free of charge. I look forward to hearing from you!
+        </p>
       </div>
     </section>
   );
